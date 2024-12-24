709d1f886a7c4f3aff5e414d2900de8392ba3274
Suri Babu Parepalli <125790360+psurigithub@users.noreply.github.com>
2024-12-24 16:28:32 +0530

    update path

--- Backend/server.js
+++ Backend/server.js
@@ -33,5 +33,5 @@ app.use("/api", contactRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on suri http://localhost:${PORT}`);
 });
